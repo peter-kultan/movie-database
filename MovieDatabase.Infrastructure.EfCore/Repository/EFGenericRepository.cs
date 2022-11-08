3c7ffd3c70740994d9af2107c6af7de791909bcd
Peter Kultan <kultanp1@gmail.com>
2022-11-08 10:56:08 +0100

    feat: add infrastructure and ef core infrastructure, repositories

---
+++ MovieDatabase.Infrastructure.EfCore/Repository/EFGenericRepository.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieDatabase.Infrastructure.Repository;
+using MovieDatabaseDAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieDatabase.Infrastructure.EfCore.Repository
+{
+    public class EFGenericRepository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        internal MovieDatabaseDbContext context;
+
+        internal DbSet<TEntity> dbSet;
+
+        public EFGenericRepository(MovieDatabaseDbContext dbContext)
+        {
+            this.context = dbContext;
+            this.dbSet = context.Set<TEntity>();
+        }
+
+        public TEntity GetById(int id)
+        {
+            return dbSet.Find(id);
+        }
+
+        public void Insert(TEntity entity)
+        {
+            dbSet.Add(entity);
+        }
+        public void Delete(TEntity entityToDelete)
+        {
+            if (context.Entry(entityToDelete).State == EntityState.Detached)
+            {
+                dbSet.Attach(entityToDelete);
+            }
+            dbSet.Remove(entityToDelete);
+        }
+
+        public async Task DeleteAsync(object id)
+        {
+            var entityToDelete = await dbSet.FindAsync(id);
+            Delete(entityToDelete);
+        }
+
+        public void Update(TEntity entityToUpdate)
+        {
+            dbSet.Attach(entityToUpdate);
+            context.Entry(entityToUpdate).State = EntityState.Modified;
+        }
+    }
+}
---
+++ MovieDatabase.Infrastructure/Repository/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieDatabase.Infrastructure.Repository
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+
+        TEntity GetById(int id);
+
+        void Insert(TEntity entity);
+
+        Task DeleteAsync(object id);
+
+        void Delete(TEntity entityToDelete);
+
+        void Update(TEntity entityToUpdate);
+    }
+}
