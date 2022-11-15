97bbaa7a56899bfd153fe4bcf9444f8935f1a92e
Peter Kultan <kultanp1@gmail.com>
2022-11-15 10:15:45 +0100

    feat: add movie service interface

---
+++ MovieDatabase.BL/Services/Interfaces/IMovieService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieDatabase.BL.Services.Interfaces
+{
+    public interface IMovieService
+    {
+
+    }
+}
