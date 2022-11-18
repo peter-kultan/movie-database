3f5888f5d8c51490314bb13372f77a13e27ef9fd
Peter Kultan <kultanp1@gmail.com>
2022-11-18 12:06:46 +0100

    feat: add ISettingsService

---
+++ MovieDatabase.BL/Services/Interfaces/ISettingsService.cs
@@ -0,0 +1,16 @@
+﻿using MovieDatabase.Shared.DTOs.Settings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieDatabase.BL.Services.Interfaces
+{
+    public interface ISettingsService
+    {
+        public SettingsDTO? LoadSettings(string path);
+
+        public void SaveSettings(SettingsDTO settings);
+    }
+}
