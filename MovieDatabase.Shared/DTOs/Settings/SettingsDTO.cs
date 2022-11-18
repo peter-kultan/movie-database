89debbedd3bc57091720ee837f26bc3e15641f88
Peter Kultan <kultanp1@gmail.com>
2022-11-18 12:06:27 +0100

    feat: add settingsDTO

---
+++ MovieDatabase.Shared/DTOs/Settings/SettingsDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieDatabase.Shared.DTOs.Settings
+{
+    public class SettingsDTO
+    {
+        public string SettingsPath { get; set; }
+
+        public string ApiKey { get; set; }
+    }
+}
